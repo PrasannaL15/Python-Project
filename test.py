c4957b313ac9568177322fe8dd56c8216a5a92f9
Prasanna <limayeprasanna15@gmail.com>
2023-11-16 15:08:20 -0500

    ,

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-print("Hello there")
+import subprocess
+
+
+def test_wc():
+    result = subprocess.run(['python', 'wc.py', 'test.txt'],
+                            capture_output=True, text=True)
+    assert result.returncode == 0
+    assert 'Words: ' in result.stdout
+    assert 'Lines: ' in result.stdout
+    assert 'Characters: ' in result.stdout
+
+
+def test_gron():
+    result = subprocess.run(['python', 'gron.py', 'test.json'],
+                            capture_output=True, text=True)
+    assert result.returncode == 0
+    assert 'json' in result.stdout
+
+
+def test_bulk_webp_converter():
+    result = subprocess.run(
+        ['python', 'bulk_webp_converter.py', '--webp', 'Images/'], capture_output=True, text=True)
+    assert result.returncode == 0
+
+
+if __name__ == '__main__':
+    test_wc()
+    test_gron()
+    test_bulk_webp_converter()
