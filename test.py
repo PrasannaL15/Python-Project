58322ffb9b1f8c781374f23ff092bb0a086c957a
Prasanna <limayeprasanna15@gmail.com>
2023-11-16 15:32:04 -0500

    Modified test.py

--- test.py
+++ test.py
@@ -1,26 +1,41 @@
 import subprocess
+import os
+import shutil
 
 
 def test_wc():
     result = subprocess.run(['python', 'wc.py', 'requirements.txt'],
                             capture_output=True, text=True)
-    print(result)
-
     assert result.returncode == 0
     assert result.stdout == '       3       8      31 requirements.txt\n'
 
 
 def test_gron():
-    result = subprocess.run(['python', 'gron.py', 'test.json'],
+    result = subprocess.run(['python', 'gron.py', 'eg.json'],
                             capture_output=True, text=True)
     assert result.returncode == 0
     assert 'json' in result.stdout
 
 
 def test_bulk_webp_converter():
+    def test_webp_images(images, output):
+
+        for image in images:
+            path = os.path.join(output, image)
+            assert os.path.exists(path)
+            assert path.endswith('.webp')
+        return True
+
+    if os.path.exists('Images/Output'):
+        shutil.rmtree('Images/Output')
+    input()
     result = subprocess.run(
-        ['python', 'bulk_webp_converter.py', '--webp', 'Images/'], capture_output=True, text=True)
+        ['python', 'bulk_webp_converter.py', '-f', 'webp', 'Images/'], capture_output=True, text=True)
+    expected_Images = ['ID.webp', 'LinkedinLogo.webp']
+
+    output_path = os.path.join('Images', 'Output')
     assert result.returncode == 0
+    assert test_webp_images(expected_Images, output_path) == True
 
 
 if __name__ == '__main__':
